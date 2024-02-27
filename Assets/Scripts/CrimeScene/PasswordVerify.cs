be858adcf83af07c885ca5c891c466214feb0af0
AnishSingha <106175433+AnishSingha@users.noreply.github.com>
2024-02-28 03:09:54 +0530

    Scenechange

--- Assets/Scripts/CrimeScene/PasswordVerify.cs
+++ Assets/Scripts/CrimeScene/PasswordVerify.cs
@@ -8,17 +8,24 @@ public class PasswordVerify : MonoBehaviour
 {
 
     [SerializeField] public TMP_InputField passWord;
-
-
+    [SerializeField] GameObject correctPassword;
+    [SerializeField] GameObject IncorrectPassword;
+    
     public void VerifyPassword()
     {
-        if (passWord.text == "Password12345")
+        if (passWord.text == "Dontdisturbme12345")
         {
             Debug.Log("Correct Password");
+            correctPassword.SetActive(true);
+            IncorrectPassword.SetActive(false);
+
         }
         else
         {
             Debug.Log("Invalid Password");
+            IncorrectPassword.SetActive(true);
+            correctPassword.SetActive(false);
+
         }
     }
 }
